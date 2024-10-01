35bf4800a57f85906b1d64eb37ef3e3966e419f6
Prashant Raj <pr2125116@gmail.com>
2024-10-01 11:36:02 +0530

    countFrequency

---
+++ countFrequency.java
@@ -0,0 +1,22 @@
+import java.util.HashMap;
+import java.util.LinkedHashMap;
+import java.util.Scanner;
+
+public class countFrequency {
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+        int n= sc.nextInt();
+        int [] arr = new int[n];
+        LinkedHashMap<Integer,Integer> hmap = new LinkedHashMap<>();
+        for(int i=0;i<n;i++){
+            arr[i] = sc.nextInt();
+            int temp = hmap.getOrDefault(arr[i],0);
+            hmap.put(arr[i],temp+1);
+        }
+
+        for(Integer key:hmap.keySet()){
+            System.out.println(key+": "+hmap.get(key) );
+        }
+
+    }
+}
