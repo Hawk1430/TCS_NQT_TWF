3ab4843adb8433a34bbfb9a9532d496d5c95e744
Prashant Raj <pr2125116@gmail.com>
2024-10-02 09:27:44 +0530

    medianArray

---
+++ medianofArray.java
@@ -0,0 +1,24 @@
+import java.util.Scanner;
+import java.util.*;
+
+public class medianofArray {
+    public static void main(String[] args){
+        Scanner sc = new Scanner(System.in);
+        int n = sc.nextInt();
+        int arr[] = new int[n];
+        for(int i=0;i<n;i++){
+            arr[i] = sc.nextInt();
+        }
+        Arrays.sort(arr);
+        double median=0;
+        if(n%2 == 0){
+            int temp = arr[(n/2)-1] ;
+            int temp2 = arr[n/2];
+            median = (double)(temp+temp2)/2;
+        }
+        else{
+            median = arr[n/2];
+        }
+        System.out.println(median);
+    }
+}
