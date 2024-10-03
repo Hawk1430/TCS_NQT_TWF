36f6a8358e63a39abfeb0e5d30852a77a8f03fad
Prashant Raj <pr2125116@gmail.com>
2024-10-03 12:57:32 +0530

    repeatingElement

---
+++ repeatingElements.java
@@ -0,0 +1,18 @@
+import java.util.LinkedHashMap;
+
+public class repeatingElements {
+    public static void main(String[] args) {
+        int arr[] = {1,1,2,3,3,4,5};
+        LinkedHashMap<Integer,Integer> lhmap = new LinkedHashMap<>();
+        for(int num: arr){
+            lhmap.put(num,lhmap.getOrDefault(num, 0) +1);
+        }
+        for(Integer key:lhmap.keySet()){
+            if(lhmap.get(key) > 1){
+                System.out.print(key+ " ");
+            }
+        }
+
+
+    }
+}
