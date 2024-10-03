82638c386b12c6e87a35e3a2282bcd3dee633709
Prashant Raj <pr2125116@gmail.com>
2024-10-03 13:32:00 +0530

    kadanes algo

---
+++ maxproductOfSubaaray.java
@@ -0,0 +1,18 @@
+public class maxproductOfSubaaray {
+    // Kadane's Algorithm
+    public static void main(String[] args) {
+        int arr[] = {1,2,-3,0,-4,-5};
+        int prod1 = arr[0];
+        int prod2 = arr[0];
+        int maxProd = arr[0];
+        for(int i=1;i<arr.length;i++){
+            int temp = Math.max(arr[i], Math.max(prod1*arr[i],prod2*arr[i]));
+            prod2 = Math.min(arr[i], Math.min(prod1*arr[i],prod2*arr[i]));
+            prod1 = temp;
+            
+            maxProd = Math.max(maxProd, prod1);
+            
+        }
+        System.out.println(maxProd);
+    }
+}
