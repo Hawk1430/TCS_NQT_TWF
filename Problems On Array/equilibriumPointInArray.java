b2e344045b63b24f62788bb8513455f1cb4ce2f8
Prashant Raj <pr2125116@gmail.com>
2024-10-05 16:26:12 +0530

    EquilibriumPointInJava

---
+++ Problems On Array/equilibriumPointInArray.java
@@ -0,0 +1,23 @@
+import java.util.*;
+
+public class equilibriumPointInArray {
+
+    public static void main(String[] args) {
+        int nums[] = { 2, 3, -1, 8, 4};
+        int totalSum = 0;
+        for (int n : nums) {
+            totalSum += n;
+        }
+        int leftSum=0;
+        int rightSum = totalSum;
+        for(int i=0;i<nums.length;i++){
+            rightSum -= nums[i];
+            if(leftSum == rightSum){
+                System.out.println(i);
+                return;
+            }
+            leftSum += nums[i];
+        }
+        System.out.println(-1);
+    }
+}
