7560a3d87c3c61d539949bb49f97814f0cb8e0a8
Prashant Raj <pr2125116@gmail.com>
2024-10-11 22:38:19 +0530

    Done

--- Problems On Array/maxproductOfSubaaray.java
+++ Problems On Array/maxproductOfSubaaray.java
@@ -7,7 +7,7 @@ public class maxproductOfSubaaray {
         int maxProd = arr[0];
         for(int i=1;i<arr.length;i++){
             int temp = Math.max(arr[i], Math.max(prod1*arr[i],prod2*arr[i]));
-            prod2 = Math.min(arr[i], Math.min(prod1*arr[i], prod2*arr[i]));
+            prod2 = Math.min(arr[i], Math.min(prod1*arr[i],prod2*arr[i]));
             prod1 = temp;
             
             maxProd = Math.max(maxProd, prod1);
