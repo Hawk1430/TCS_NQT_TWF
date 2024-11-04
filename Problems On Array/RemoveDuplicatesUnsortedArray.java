236c39baecd1e85ba75051a1f6388d45e8e46679
Prashant Raj <pr2125116@gmail.com>
2024-11-04 16:17:18 +0530

    DOne

--- Problems On Array/RemoveDuplicatesUnsortedArray.java
+++ Problems On Array/RemoveDuplicatesUnsortedArray.java
@@ -7,7 +7,7 @@ public class RemoveDuplicatesUnsortedArray {
         for(int i=0;i<arr.length;i++){
             if(!hmap.containsKey(arr[i])){
                 System.out.print(arr[i]+ " ");
-                hmap.put( arr[i],1);
+                hmap.put(arr[i],1);
             }
             
         }
